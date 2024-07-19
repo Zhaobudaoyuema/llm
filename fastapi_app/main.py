c539377bab70a2571f736e5a89a3ac48e6106723
zhaobudaoyuanma <agentn615@gmail.com>
2024-07-19 21:03:59 +0800

    题解readme

---
+++ fastapi_app/main.py
@@ -0,0 +1,26 @@
+from typing import Annotated, Union
+
+import uvicorn
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=1
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: Union[float, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
